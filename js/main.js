8de8967f89b583e4a29dfdee874b48a618527272
Arturs Polis <arturs.polis@gmail.com>
2018-04-23 15:47:30 +0300

    Added support for changing years and updating the map

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
       // coordinates
       projection = d3.geoNaturalEarth1(),
       theworld = void 0;
+      countryData = void 0;
+      cityData = void 0;
 
   // Special d3 helper that converts geo coordinates to paths
   // based on a projection 
@@ -17,23 +19,19 @@
       .attr("height", height);
 
   var hoverTooltip = function(d) {
-    console.log('d', d, 'event', d3.event);
+    //console.log('d', d, 'event', d3.event);
     var div = document.getElementById('tooltip');
     // d3.event is the current event
     div.style.left = d3.event.pageX +'px'; 
     div.style.top = d3.event.pageY + 'px';
-    div.innerHTML = d.name;
+    div.innerHTML = d.city;
   };
-  // TODO:
-  // Draw a legend box
-  // Convert on load things to more elegant function
 
-  d3.json('data/110m.json').then(function(data) {
-
+  var showMap = function(data) {
     console.log('theworld', data);
     // Get the "polygon" data for all countries
     var countries = topojson.feature(data, data.objects.countries);
-    console.log('countries', countries)
+    //console.log('countries', countries)
     // Setup the scale and translate
     var b, s, t; // bounds, scale, translate?
     projection.scale(1).translate([0, 0]);
@@ -47,8 +45,8 @@
     var vpadding = 0.05
     var hpadding = -0.05
     var t = [hpadding * width + (width - s * (b[1][0] + b[0][0])) / 2, vpadding*height + (height - s * (b[1][1] + b[0][1])) / 2];
-    console.log("s:", s)
-    console.log("t:", t)
+    //console.log("s:", s)
+    //console.log("t:", t)
     // First we scale (it was scaled with respect to (0,0))
     // Next we translate - to center the map at the origin
     projection.scale(s).translate(t);
@@ -77,47 +75,70 @@
 
     //Exit - not used at the momeent
     theworld.exit().remove();
-    d3.csv('data/cities_1950.csv').then(function(cities){
-      var cityPoints = svg.selectAll('circle').data(cities);
-      var cityText = svg.selectAll('text').data(cities);
+  };
 
-      console.log(cities)
+  var updateCities = function(data, year) {    
 
-      cityPoints.enter() // remember - for each data point!
-        .append('circle')
-        .attr('cx', function(d) {
-          return projection ([d.lon, d.lat])[0]
-        })
-        .attr('cy', function(d) {
-          return projection ([d.lon, d.lat])[1]
-        })
-        .attr('r', function(d) {
-          return Math.sqrt(Math.floor(200*d.pop)/Math.PI)
-        })
-        .attr('fill', 'red')
-        .on('mouseover', hoverTooltip);
+    var cityPoints = svg.selectAll('circle').data(data.cities[year]);
+    var cityText = svg.selectAll('text').data(data.cities[year]);
 
-      cityText.enter()
-        .append('text')
-        .attr('x', function(d) {
-          return projection ([d.lon, d.lat])[0]
-        })
-        .attr('y', function(d) {
-          return projection ([d.lon, d.lat])[1]
-        })
-        .attr('dx', 5)
-        .attr('dy', 3)
-        //.text(function(d) {return d.name});
-    });
+    console.log(svg)
+    console.log(data.cities[year])
 
+    cityPoints.exit().remove();
 
-  });
+    cityPoints.enter() // remember - for each data point!
+      .append('circle')
+      .attr('cx', function(d) {
+        return projection ([d.lon, d.lat])[0]
+      })
+      .attr('cy', function(d) {
+        return projection ([d.lon, d.lat])[1]
+      })
+      .attr('r', function(d) {
+        return Math.sqrt(Math.floor(200*d.raw_pop)/Math.PI)
+      })
+      .attr('fill', 'red')
+      .on('mouseover', hoverTooltip);
 
-  var updateMap = function(year) {
-    // Load the data for the year year
-    return 0
-  }
+    cityPoints
+      .attr('cx', function(d) {
+        return projection ([d.lon, d.lat])[0]
+      })
+      .attr('cy', function(d) {
+        return projection ([d.lon, d.lat])[1]
+      })
+      .attr('r', function(d) {
+        return Math.sqrt(Math.floor(200*d.raw_pop)/Math.PI)
+      })
+      .attr('fill', 'red')
+      .on('mouseover', hoverTooltip);
+  };
+
+  var showTheWorld = function(data) {
+    countryData = data[0]
+    cityData = data[1]
+    //console.log('countryData', countryData);
+    //console.log('cityData', cityData);
+    showMap(countryData);
+    updateCities(cityData, '1955');
+  };
+
+  // Define which files we need to load:
+  var dataPaths = ['data/110m.json', 'data/json_final.json']
+  var promises = [];
+
+  // Load the JSON files:
+  dataPaths.forEach(function(dataPath){
+    promises.push(d3.json(dataPath));
+  })
+
+  // Show the map once all the jsons are loaded:
+  Promise.all(promises).then(showTheWorld);
 
   var yearChanger = d3.select("#changeYearBtn")
-    .on('click', function(){ alert('foo'); });
+    .on('click', function(){ 
+      var year = document.getElementById("inputYear").value;
+      updateCities(cityData, year);
+    });
 })();
